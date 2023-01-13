1815bf19fba5d99b2a2d8f866b460fde1ae4c216
feniljain <fkjainco@gmail.com>
2023-01-13 08:19:15 +0530

    feat: add support for `exec` command
    add `thiserror` and create a global `ShellError` type
    for starting the process of unification of errors

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,9 @@ use std::{
         Arc,
     },
 };
+use thiserror::Error;
 
-const BUILTIN_COMMANDS: [&str; 1] = ["cd"];
+const BUILTIN_COMMANDS: [&str; 2] = ["cd", "exec"];
 
 #[derive(Default, Clone, Debug)]
 struct Command {
@@ -77,8 +78,15 @@ enum Color {
     Red,
 }
 
-// FIXME: Handle error properly everywhere
+#[derive(Error, Debug)]
+enum ShellError {
+    #[error("dss: command not found: {0}\n")]
+    CommandNotFound(String),
+}
+
+// FIXME: Handle error properly everywhere using ShellError
 // FIXME: Remove all unnecessary clones
+// FIXME: Refine APIs exposed by Engine and Command
 
 // Tasks
 // [X] correct command split by space
@@ -146,10 +154,12 @@ fn main() -> anyhow::Result<()> {
         let (commands, separators) = Command::parse_input(input_str);
         if separators.len() == 0 {
             assert!(commands.len() == 1);
+
             if commands[0].args_with_cmd[0] == "exit" {
                 break;
             }
-            engine.execute_command(commands[0].clone())?;
+
+            engine.execute_command_by_forking(commands[0].clone())?;
         } else {
             let mut execution_result: Option<(bool, &Separator)> = None;
             let n_cmds = commands.len();
@@ -166,16 +176,16 @@ fn main() -> anyhow::Result<()> {
                     Some((last_execution_result, separator)) => {
                         match separator {
                             Separator::Semicolon => {
-                                let _ = engine.execute_command(command.clone());
+                                let _ = engine.execute_command_by_forking(command.clone());
                             }
                             Separator::LogicalOr => {
                                 if !last_execution_result {
-                                    let _ = engine.execute_command(command.clone());
+                                    let _ = engine.execute_command_by_forking(command.clone());
                                 }
                             }
                             Separator::LogicalAnd => {
                                 if last_execution_result {
-                                    let _ = engine.execute_command(command.clone());
+                                    let _ = engine.execute_command_by_forking(command.clone());
                                 }
                             }
                         }
@@ -185,7 +195,7 @@ fn main() -> anyhow::Result<()> {
                         }
                     }
                     None => {
-                        let _ = engine.execute_command(command.clone());
+                        let _ = engine.execute_command_by_forking(command.clone());
                         execution_result = Some((engine.execution_successful, &separators[i]));
                     }
                 }
@@ -319,18 +329,9 @@ impl Engine {
         }
     }
 
-    fn execute_command(&mut self, command: Command) -> anyhow::Result<()> {
+    fn execute_command_by_forking(&mut self, command: Command) -> anyhow::Result<()> {
         if is_builtin_command(&command.args_with_cmd[0]) {
-            let mut path_to_go_str = "/";
-            if command.args.len() > 1 {
-                // If we receive `~` after cd, we want to go to
-                // absolute root, which is what "/" denotes already
-                if command.args_with_cmd[1] != "~" {
-                    path_to_go_str = &command.args_with_cmd[1];
-                }
-            }
-
-            let result = handle_builtin_command(&command.args_with_cmd[0], path_to_go_str);
+            let result = self.handle_builtin_command(command);
             if result.is_err() {
                 self.execution_successful = false;
             } else {
@@ -362,63 +363,99 @@ impl Engine {
                     }
                 }
                 Ok(ForkResult::Child) => {
-                    // FIXME: Optimize this .len() out,
-                    // we just wanna know if there are more
-                    // than 1 elements
-                    let args: &[CString] = if command.args.len() < 1 {
-                        &[]
-                    } else {
-                        &command.args
-                    };
-
-                    let mut exit_status = 0;
-                    let mut errno_opt: Option<Errno> = None;
-                    // If command starts with "/" or "./" or "../", do not do PATH appending
-                    if command.is_unqualified_path {
-                        'env_paths: for env_path_str in &self.env_paths {
-                            let mut path = PathBuf::from_str(&env_path_str)
-                                .expect("Could not construct path buf from env_path");
-
-                            path.push(command.path.clone());
-
-                            match execve_(&path, args) {
-                                // This Ok() break is actually useless
-                                // cause execve() only returns if there's
-                                // an error, otherwise it just stops the
-                                // child and returns the control to
-                                // parent. For more understanding
-                                // read: RETURN VALUES section
-                                // of execve man page
-                                Ok(_) => break 'env_paths,
-                                Err(errno_) => {
-                                    errno_opt = Some(errno_);
-                                }
-                            }
-                        }
-                    } else {
-                        let result = execve_(&command.path, args);
-                        if let Err(errno) = result {
-                            errno_opt = Some(errno);
-                        }
-                    }
-
-                    if let Some(errno) = errno_opt {
-                        write_error_to_shell(
-                            errno,
-                            &command.args_with_cmd[0],
-                            command.is_unqualified_path,
-                        )?;
-                        // FIXME: Pass proper errno here
-                        exit_status = 1;
-                    }
-
-                    unsafe { libc::_exit(exit_status) };
+                    // FIXME: Handle error here
+                    self.execute_command_without_forking(command)?;
                 }
                 Err(err) => panic!("Fork failed: {err:?}"),
             }
         }
 
         Ok(())
+    }
+
+    // GOTCHA: This currently executes the command and stops the complete program
+    // due to libc::exit at the end
+    fn execute_command_without_forking(&mut self, command: Command) -> anyhow::Result<()> {
+        // FIXME: Optimize this .len() out,
+        // we just wanna know if there are more
+        // than 1 elements
+        let args: &[CString] = if command.args.len() < 1 {
+            &[]
+        } else {
+            &command.args
+        };
+
+        let mut exit_status = 0;
+        let mut errno_opt: Option<Errno> = None;
+        // If command starts with "/" or "./" or "../", do not do PATH appending
+        if command.is_unqualified_path {
+            'env_paths: for env_path_str in &self.env_paths {
+                let mut path = PathBuf::from_str(&env_path_str)
+                    .expect("Could not construct path buf from env_path");
+
+                path.push(command.path.clone());
+
+                match execve_(&path, args) {
+                    // This Ok() break is actually useless
+                    // cause execve() only returns if there's
+                    // an error, otherwise it just stops the
+                    // child and returns the control to
+                    // parent. For more understanding
+                    // read: RETURN VALUES section
+                    // of execve man page
+                    Ok(_) => break 'env_paths,
+                    Err(errno_) => {
+                        errno_opt = Some(errno_);
+                    }
+                }
+            }
+        } else {
+            let result = execve_(&command.path, args);
+            if let Err(errno) = result {
+                errno_opt = Some(errno);
+            }
+        }
+
+        if let Some(errno) = errno_opt {
+            write_error_to_shell(
+                errno,
+                &command.args_with_cmd[0],
+                command.is_unqualified_path,
+            )?;
+            // FIXME: Pass proper errno here
+            exit_status = 1;
+        }
+
+        unsafe { libc::_exit(exit_status) };
+    }
+
+    fn handle_builtin_command(&mut self, mut command: Command) -> anyhow::Result<()> {
+        let cmd_str = command.args_with_cmd[0].as_str();
+
+        match cmd_str {
+            "cd" => {
+                let mut path_to_go_str = "/";
+                if command.args.len() > 1 {
+                    // If we receive `~` after cd, we want to go to
+                    // absolute root, which is what "/" denotes already
+                    if command.args_with_cmd[1] != "~" {
+                        path_to_go_str = &command.args_with_cmd[1];
+                    }
+                }
+
+                let cmd_path = Path::new(path_to_go_str);
+                chdir(cmd_path)?;
+                Ok(())
+            }
+            "exec" => {
+                // Remove `exec` keyword and then pass the remaining command
+                command.args_with_cmd.remove(0);
+                let command = Command::parse_cmd_str_vec(command.args_with_cmd);
+                self.execute_command_without_forking(command)?;
+                Ok(())
+            },
+            _ => Err(ShellError::CommandNotFound(cmd_str.to_string()).into()),
+        }
     }
 }
 
@@ -438,15 +475,6 @@ fn execve_(path: &PathBuf, args: &[CString]) -> nix::Result<Infallible> {
 
 fn is_builtin_command(cmd: &str) -> bool {
     BUILTIN_COMMANDS.contains(&cmd)
-}
-
-fn handle_builtin_command(cmd_str: &str, path_to_go_str: &str) -> anyhow::Result<()> {
-    if cmd_str == "cd" {
-        let cmd_path = Path::new(path_to_go_str);
-        chdir(cmd_path)?;
-    }
-
-    Ok(())
 }
 
 fn parse_paths() -> Vec<String> {
